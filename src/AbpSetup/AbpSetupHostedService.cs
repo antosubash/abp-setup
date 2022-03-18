eabcb75a986d9f049f1e32790d2fdc9f6603e586
Anto Subash <antosubash@live.com>
2022-03-18 10:41:12 +0100

    console app init

---
+++ src/AbpSetup/AbpSetupHostedService.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Volo.Abp;
+
+namespace AbpSetup;
+
+public class AbpSetupHostedService : IHostedService
+{
+    private IAbpApplicationWithInternalServiceProvider _abpApplication;
+
+    private readonly IConfiguration _configuration;
+    private readonly IHostEnvironment _hostEnvironment;
+
+    public AbpSetupHostedService(IConfiguration configuration, IHostEnvironment hostEnvironment)
+    {
+        _configuration = configuration;
+        _hostEnvironment = hostEnvironment;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _abpApplication =  await AbpApplicationFactory.CreateAsync<AbpSetupModule>(options =>
+        {
+            options.Services.ReplaceConfiguration(_configuration);
+            options.Services.AddSingleton(_hostEnvironment);
+
+            options.UseAutofac();
+            options.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog());
+        });
+
+        await _abpApplication.InitializeAsync();
+
+        var helloWorldService = _abpApplication.ServiceProvider.GetRequiredService<HelloWorldService>();
+
+        await helloWorldService.SayHelloAsync();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _abpApplication.ShutdownAsync();
+    }
+}
---
+++ src/AbpSetup/HelloWorldService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Volo.Abp.DependencyInjection;
+
+namespace AbpSetup;
+
+public class HelloWorldService : ITransientDependency
+{
+    public ILogger<HelloWorldService> Logger { get; set; }
+
+    public HelloWorldService(ILogger<HelloWorldService> logger)
+    {
+        Logger = logger;
+    }
+
+    public Task SayHelloAsync()
+    {
+        Logger.LogInformation("Hello World!");
+        return Task.CompletedTask;
+    }
+}
